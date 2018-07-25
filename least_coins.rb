20410aa0878d690198229a7499d95e6642b09ee7
mvchfvn <mimi123meg@gmail.com>
2018-07-25 16:39:22 +0000

    Done.

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,16 @@
 #write out your code here
 
 def least_coins(cents)
-
-#Code your answer here!
-
+  coins = {}
+  coins[:quarters] = cents/25
+  cents = cents % 25
+  coins[:dimes]= cents/10 
+  cents = cents % 10 
+  coins[:nickels] = cents/5 
+  cents = cents% 5
+  coins[:pennies] = cents
+  return coins
 end
+
+puts least_coins(29)
+
